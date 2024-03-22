0fcdf89745f12797bb0dd12e66b168fea9b68c3b
Ahmet DALDEMİR <21971399+ahmetdaldemir@users.noreply.github.com>
2024-03-22 23:45:00 +0300

    firsrt

---
+++ app/Models/UserSallary.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Scopes\CompanyScope;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class UserSallary extends Model
+{
+    use HasFactory,SoftDeletes;
+
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new CompanyScope);
+    }
+
+}
---
+++ database/migrations/2024_03_22_232827_create_user_sallaries_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_sallaries', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->double('price',10,2)->default(0);
+            $table->string('month')->default(\Carbon\Carbon::now()->month);
+            $table->year('year')->default(\Carbon\Carbon::now()->year);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_sallaries');
+    }
+};
