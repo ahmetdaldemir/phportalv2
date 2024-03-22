9868e3c996540576e5fd361a1a57aca160e0964f
Ahmet DALDEMİR <21971399+ahmetdaldemir@users.noreply.github.com>
2024-03-22 23:54:30 +0300

    firsrt

--- database/migrations/2024_03_22_232827_create_user_sallaries_table.php
+++ database/migrations/2024_03_22_232827_create_user_sallaries_table.php
@@ -16,8 +16,12 @@ return new class extends Migration
         Schema::create('user_sallaries', function (Blueprint $table) {
             $table->unsignedBigInteger('id')->autoIncrement();
 
+            $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
 
             $table->double('price',10,2)->default(0);
             $table->string('month')->default(\Carbon\Carbon::now()->month);
