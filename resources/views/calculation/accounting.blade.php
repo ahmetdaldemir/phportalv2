48da1e9d676f19b55b23323eb631d9c7fe8d5ea7
Ahmet DALDEMİR <21971399+ahmetdaldemir@users.noreply.github.com>
2024-03-22 22:43:40 +0300

    firsrt

--- resources/views/calculation/accounting.blade.php
+++ resources/views/calculation/accounting.blade.php
@@ -105,7 +105,7 @@
                             @foreach($finantransactions as $finantransaction)
 
                                 <tr>
-                                    <td>{{\App\Models\FinansTransaction::MODEL_STRING[$finantransaction->model_class]}}</td>
+                                    <td>{{\App\Models\FinansTransaction::MODEL_STRING[''.$finantransaction->model_class.'']}}</td>
                                     <td>{{$finantransaction->model_id}}</td>
                                     <td>{{$finantransaction->price}}</td>
                                     <td>{{$finantransaction->created_date}}</td>
